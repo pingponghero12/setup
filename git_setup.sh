13f8b73d115a6ef20cc66fb8632e29497749c907
Manfred Gawlas <manfredgawlas12@gmail.com>
2024-07-26 16:54:07 +0200

    git_setup

---
+++ git_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p ~/.ssh
+
+ansible-vault decrypt ~/Documents/setup/id_ed25519 --output ~/.ssh/id_ed25519
+
+ansible-vault decrypt ~/Documents/setup/id_ed25519.pub --output ~/.ssh/id_ed25519.pub
+
+chmod 600 ~/.ssh/id_ed25519
+chmod 644 ~/.ssh/id_ed25519.pub
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/id_ed25519
+
+ssh -T git@github.com
